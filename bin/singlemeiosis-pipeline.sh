39c8b66180746d40f4591c588ae7fe87cd511e35
Joseph Tran <Joseph.Tran@versailles.inra.fr>
2013-10-21 15:03:27 +0200

    declare variables as local

--- bin/singlemeiosis-pipeline.sh
+++ bin/singlemeiosis-pipeline.sh
@@ -8,7 +8,7 @@
 
 # Date: 2013-10-11
 
-VERSION=dev
+local VERSION=dev
 
 ########################
 # SECTION CONFIGURATION
@@ -16,47 +16,47 @@ VERSION=dev
 
 # Include lib functions
 
-PROD_PREFIX="/usr/local"
-DEV_PREFIX="$(pwd)/.."
-PREFIX=$DEV_PREFIX # TO BE CHANGED WHEN SWITCHING TO PROD
+local PROD_PREFIX="/usr/local"
+local DEV_PREFIX="$(pwd)/.."
+local PREFIX=$DEV_PREFIX # TO BE CHANGED WHEN SWITCHING TO PROD
 
 . $PROD_PREFIX/share/bash-common/lib/bash-common_lib.inc
 . $PREFIX/share/singlemeiosis-pipeline/lib/singlemeiosis-pipeline_lib.inc
 
 # CLI ARGUMENTS
 
-ARGS=3
-SeqFile1=$1
-SeqFile2=$3
-SampleName=$3
+local ARGS=3
+local SeqFile1=$1
+local SeqFile2=$3
+local SampleName=$3
 
 # SESSION VARIABLES
 
-DATE=$(date '+%F_%Hh%Mm%Ss')
-SESSION_ID=$(date '+%Y%M%d%H%M%S')
-EXECUTED_COMMAND="$0 $*"
+local DATE=$(date '+%F_%Hh%Mm%Ss')
+local SESSION_ID=$(date '+%Y%M%d%H%M%S')
+local EXECUTED_COMMAND="$0 $*"
 
-NAMESPACE="SINGLEMEIOSIS"
-JOB_TAG=$NAMESPACE_$SampleName
-SESSION_TAG=${JOB_TAG}_${USER}_${SESSION_ID}
+local NAMESPACE="SINGLEMEIOSIS"
+local JOB_TAG=$NAMESPACE_$SampleName
+local SESSION_TAG=${JOB_TAG}_${USER}_${SESSION_ID}
 
-WORKING_DIR=$(pwd)
-SESSION_DIR=$SESSION_TAG
-LOG_DIR=$SESSION_DIR/"log"
-LOGFILE=${SESSION_TAG}.log
+local WORKING_DIR=$(pwd)
+local SESSION_DIR=$SESSION_TAG
+local LOG_DIR=$SESSION_DIR/"log"
+local LOGFILE=${SESSION_TAG}.log
 
-ERROR_TMP="/tmp/$(basename ${0%.*})_error_${SESSION_TAG}.log"
+local ERROR_TMP="/tmp/$(basename ${0%.*})_error_${SESSION_TAG}.log"
 
 
 # CONFIG PARAMETERS LIST TO BE MOVED TO CONFIG FILE: cf ../share/singlemeiosis-pipeline/etc/singlemeiosis-pipeline_user.config
 
-PIPELINE_SHARED=$PREFIX/share/$(basename ${0%.*})
-PROD_PIPELINE_USER_CONFIG=$WORKING_DIR/$(basename ${0%.*})_user.config
-DEV_PIPELINE_USER_CONFIG=$PIPELINE_SHARED/etc/$(basename ${0%.*})_user.config
-PIPELINE_USER_CONFIG=$DEV_PIPELINE_USER_CONFIG # TO BE CHANGED WHEN SWITCHING TO PROD
+local PIPELINE_SHARED=$PREFIX/share/$(basename ${0%.*})
+local PROD_PIPELINE_USER_CONFIG=$WORKING_DIR/$(basename ${0%.*})_user.config
+local DEV_PIPELINE_USER_CONFIG=$PIPELINE_SHARED/etc/$(basename ${0%.*})_user.config
+local PIPELINE_USER_CONFIG=$DEV_PIPELINE_USER_CONFIG # TO BE CHANGED WHEN SWITCHING TO PROD
 
 # GET GENOME ALIASES LIST
-GENOME_ALIASES_LIST=($(get_genome_aliases_list_wo_snpeff $PIPELINE_USER_CONFIG 2>$ERROR_TMP))
+local GENOME_ALIASES_LIST=($(get_genome_aliases_list_wo_snpeff $PIPELINE_USER_CONFIG 2>$ERROR_TMP))
 # TODO: print a warning if error
 
 #===============
